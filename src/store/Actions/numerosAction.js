74c79197eb875f4d2661996ca7918c8fefd1df70
hugolfsimoes <hugolfsimoes@gmail.com>
2022-04-28 19:22:54 -0300

    colocando as actiontypes em constantes

--- src/store/Actions/numerosAction.js
+++ src/store/Actions/numerosAction.js
@@ -1,15 +1,15 @@
-
+import { NUM_MAX_ALTERADO, NUM_MIN_ALTERADO } from './actionTypes';
 //Action Creator
 export const alterarNumeroMinimo = (novoNumero) => {
   return {
-    type: 'NUM_MIN_ALTERADO',
+    type: NUM_MIN_ALTERADO,
     payload: novoNumero
   };
 };
 
 export const alterarNumeroMaximo = (novoNumero) => {
   return {
-    type: 'NUM_MAX_ALTERADO',
+    type: NUM_MAX_ALTERADO,
     payload: novoNumero
   };
 };
--- src/store/Reducers/numerosReducer.js
+++ src/store/Reducers/numerosReducer.js
@@ -1,4 +1,4 @@
-
+import { NUM_MAX_ALTERADO, NUM_MIN_ALTERADO } from '../Actions/actionTypes';
 const initialState = {
   min: 1,
   max: 10
@@ -7,12 +7,12 @@ const initialState = {
 
 export default function numerosReducer(state = initialState, action) {
   switch (action.type) {
-    case 'NUM_MIN_ALTERADO':
+    case NUM_MIN_ALTERADO:
       return {
         ...state,
         min: action.payload
       };
-    case 'NUM_MAX_ALTERADO':
+    case NUM_MAX_ALTERADO:
       return {
         ...state,
         max: action.payload
